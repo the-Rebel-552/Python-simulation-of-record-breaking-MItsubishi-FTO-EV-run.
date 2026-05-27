f20bdd7bd7e142cdb4437f038baee8bfd1ce10ad
the-Rebel-552 <rebelthemachine@gmail.com>
2026-05-27 15:32:36 +0200

    Update Calc_CARS_EN.py
    Updating instructions in end of the file

--- Calc_CARS_EN.py
+++ Calc_CARS_EN.py
@@ -417,26 +417,26 @@ result_distance = calc_distance(
 )
 
 """
-  Comments — #---hidden for the finale
+Comments — #---hidden for the finale
     these parts of the code are hidden for the final version of the program
     can be opened to see how interval calculations work.
-
-    To calculate an unknown acceleration time (for example, the acceleration time to 130 km/h when
-    you know the acceleration to 100 km/h):
-      you need to open (#---hidden for the finale) in the calc_non_uniform_accel_distance function, set more intervals
-      and then do the acceleration calculations according to the instructions:
-        if the car 0-100 in 12.5 seconds then to 130 in approximately 20.495 seconds (Lancer 1.6: 0-100 = 12.5, 0-130 = 20.5)
-        to do this, you need to take the last segment of acceleration to 100 and count how many times
-        it will reach the required speed and multiply by a time slightly greater than the time of the same segment
-          For example, the acceleration of the last segment was as follows:
-            Interval 50 (t=0.39 seconds):
-              V: 27.22 -> 27.78 m/s | a: 1.42 m/s² | s: 10.78 m
-              V: 98.00 -> 100.00 km/h | - here the speed increase is 2 km/h
-            1 - Take the time t=0.39 s, increase it by 30%, to 0.533 s (number of % = speed difference you need to calc)
-            2 - Find out how many such segments will fit in the acceleration to the desired speed (here 15)
-                (130 - 100) / 2 = 15 (this is the figure of the speed increase for one segment of the calculation)
-            3 - 15 * 0.533 = 7.995 seconds
-            4 - then add this acceleration to the acceleration to 100 km/h 12.5 + 7.995 = 20.5 s
+    To calculate an unknown acceleration time (for example, the acceleration time to 130 km/h when you know the acceleration to 100 km/h):
+      you need to open (#---hidden for the finale) in the calc_non_uniform_accel_distance function
+      set the known acceleration of the car to 100 km/h in Characteristics of the car section
+      and then do the acceleration calculations according to the instructions
+      (if the car 0-100 in 12.5 seconds then to 130 in approximately 20.495 seconds (Lancer 1.6: 0-100 = 12.5, 0-130 = 20.5)).
+        Instructions to do this calc:
+          1- Run the program
+          2- Take the last segment of acceleration to 100 and count how many times it will reach the required speed and multiply by a time slightly greater than the time of the same segment
+            For example, the acceleration of the last segment was as follows:
+                          Interval 50 (t=0.39 seconds):
+                            V: 27.22 -> 27.78 m/s | a: 1.42 m/s² | s: 10.78 m
+                            V: 98.00 -> 100.00 km/h | - here the speed increase is 2 km/h
+                1 - Take the time t=0.39 s, increase it by 30%, to 0.533 s (number of % = speed difference you need to calc)
+                2 - Find out how many such segments will fit in the acceleration to the desired speed (here 15)
+                    (130 - 100) / 2 = 15 (this is the figure of the speed increase for one segment of the calculation)
+                3 - 15 * 0.533 = 7.995 seconds
+                4 - then add this acceleration to the acceleration to 100 km/h 12.5 + 7.995 = 20.5 s
 """
 
 """
